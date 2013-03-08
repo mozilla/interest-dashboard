5e6b37b315ca7a254befb44b3db7d1cb49fcc8cf
Edward Lee <edilee@mozilla.com>
2013-03-08 12:50:28 -0800

    Switch code to a basic add-on that can display interests.

---
+++ data/profile.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+self.port.on("style", function(file) {
+  let link = document.createElement("link");
+  link.setAttribute("href", file);
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  document.head.appendChild(link);
+});
