8077aa9973782b66335099cd50dfcbf15c0f3bad
Edward Lee <edilee@mozilla.com>
2013-04-01 15:20:19 -0700

    Add some buttons to recompute various number of days of history.

--- data/profile.js
+++ data/profile.js
@@ -2,6 +2,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Add some buttons to recompute some amount of history
+const recomputeDays = [0, 3, 7, 15, 30, 60, 120, 240];
+recomputeDays.forEach(days => {
+  let parentDiv = document.getElementById("recompute");
+  let button = document.createElement("button");
+  button.textContent = days + " days";
+  parentDiv.appendChild(button);
+
+  // Don't allow more clicks and start recomputing
+  button.addEventListener("click", () => {
+    parentDiv.classList.add("disabled");
+    self.port.emit("call_service", "resubmitRecentHistoryVisits", [days]);
+  });
+});
+
 // Call the service to get the buckets and display results
 self.port.emit("call_service", "_getTopInterests", [100]);
 
@@ -9,6 +24,11 @@ self.port.emit("call_service", "_getTopInterests", [100]);
 self.port.on("called_service", function(method, args, result) {
   this["on" + method](args, result);
 });
+
+// Reload the page on a completed resubmit
+function onresubmitRecentHistoryVisits(args, result) {
+  location.reload();
+}
 
 // Update the table with top interests
 function on_getTopInterests(args, result) {
