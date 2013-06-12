b71bc034f3c492980beee57ab3b99ff57238a013
Edward Lee <edilee@mozilla.com>
2013-06-12 08:13:40 -0700

    Change to domain processing.

--- data/profile.js
+++ data/profile.js
@@ -2,59 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Add some buttons to recompute some amount of history
-const recomputeDays = [0, 3, 7, 15, 30, 60, 120, 240];
-recomputeDays.forEach(days => {
-  let parentDiv = document.getElementById("recompute");
-  let button = document.createElement("button");
-  button.textContent = days + " days";
-  parentDiv.appendChild(button);
+let daysVisited;
+self.port.on("daysVisited", data => {
+  daysVisited = data;
 
-  // Don't allow more clicks and start recomputing
-  button.addEventListener("click", () => {
-    parentDiv.classList.add("disabled");
-    self.port.emit("call_service", "resubmitRecentHistoryVisits", [days]);
-  });
+  let sortedKeys = Object.keys(data).sort((a, b) => data[b] - data[a]);
+  let domainText = sortedKeys.map(key => key + " " + data[key]).join("<br>");
+  document.getElementById("domains").innerHTML = domainText;
+
+  updateOutput();
 });
-
-// Call the service to get the buckets and display results
-self.port.emit("call_service", "getInterestsByNamespace", ["", {interestLimit: 100}]);
-
-// Dispatch service responses to the appropriate function
-self.port.on("called_service", function(method, args, result) {
-  this["on" + method](args, result);
-});
-
-// Reload the page on a completed resubmit
-function onresubmitRecentHistoryVisits(args, result) {
-  location.reload();
-}
-
-// Update the table with top interests
-function ongetInterestsByNamespace(args, result) {
-  let table = document.getElementById("interestTable");
-  result.forEach(({diversity, meta, name, recency, score}) => {
-    let {duration, sharable, threshold} = meta;
-    let {immediate, recent, past} = recency;
-    let tr = document.createElement("tr");
-    tr.innerHTML = "<td>" + name + "</td>" +
-      "<td>" + immediate + "</td>" +
-      "<td>" + recent + "</td>" +
-      "<td>" + past + "</td>" +
-      "<td>" + score + "</td>" +
-      "<td>" + diversity + "</td>" +
-      "<td><input type=\"checkbox\"/></td>";
-    table.appendChild(tr);
-
-    // Set the share status and allow toggling
-    let checkbox = tr.lastChild.firstChild;
-    checkbox.checked = sharable;
-    checkbox.addEventListener("click", event => {
-      let action = checkbox.checked ? "_unsetIgnoredForInterest" : "_setIgnoredForInterest";
-      self.port.emit("call_service", action, [name]);
-    });
-  });
-}
 
 self.port.on("style", function(file) {
   let link = document.createElement("link");
@@ -63,3 +20,21 @@ self.port.on("style", function(file) {
   link.setAttribute("type", "text/css");
   document.head.appendChild(link);
 });
+
+function updateOutput() {
+  let output = [];
+
+  let categoryData = document.getElementById("categories").value.trim().split("\n");
+  categoryData.forEach(line => {
+    let domains = line.split(/\s+/);
+    let name = domains.shift();
+
+    let itemDays = domains.map(key => daysVisited[key] || 0);
+    let totalDays = itemDays.reduce((item, sum) => sum + item, 0);
+    output.push(name + ": " + totalDays + " = " + itemDays.join(" + "));
+  });
+
+  document.getElementById("output").value= output.join("\n");
+}
+
+document.getElementById("categories").addEventListener("input", updateOutput);
