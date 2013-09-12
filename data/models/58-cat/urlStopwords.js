99f766e7c556f09d64a522c3cfde0d5dda7129f6
Olivier Yiptong <olivier@olivieryiptong.com>
2013-09-12 11:52:05 -0400

    58-category data inspired from blekko, alexa and other sources

---
+++ data/models/58-cat/urlStopwords.js
@@ -0,0 +1,21 @@
+var interestsUrlStopwords = {
+  "cgi": true, 
+  "index": true, 
+  "cfm": true, 
+  "shtml": true, 
+  "home": true, 
+  "htm": true, 
+  "welcome": true, 
+  "html": true, 
+  "js": true, 
+  "default": true, 
+  "asp": true, 
+  "pl": true, 
+  "wml": true, 
+  "php4": true, 
+  "php5": true, 
+  "php": true, 
+  "aspx": true, 
+  "css": true, 
+  "wap": true
+}
