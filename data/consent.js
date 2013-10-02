83c3c321163c0ae2af9416cd848f2f0341b37afd
Olivier Yiptong <olivier@olivieryiptong.com>
2013-10-02 18:58:13 -0400

    allow viewing of raw data on consent page

--- data/consent.js
+++ data/consent.js
@@ -1,35 +1,62 @@
 "use strict";
 
-let DataService = function($rootScope) {
-  this.rootScope = $rootScope;
-
-  // relay messages from the addon to the page
+let consentMenu = angular.module("consentMenu", ["ui.bootstrap"]);
+consentMenu.controller("consentCtrl", function($scope, $modal) {
   self.port.on("message", message => {
-    this.rootScope.$apply(_ => {
-      this.rootScope.$broadcast(message.content.topic, message.content.data);
+    $scope.$apply(_ => {
+      $scope.$broadcast(message.content.topic, message.content.data);
     });
   });
-}
 
-DataService.prototype = {
-  send: function _send(message) {
-    self.port.emit(message);
-  },
-}
+  $scope.dispatchBatch = null;
 
-let consentMenu = angular.module("consentMenu", []);
-consentMenu.service("dataService", DataService);
-
-consentMenu.controller("consentCtrl", function($scope, dataService) {
   /** UI functionality **/
   $scope.acceptStudy = function() {
-    dataService.send("survey_run");
+    self.port.emit("survey_run");
   }
 
   $scope.rejectStudy = function() {
-    dataService.send("uninstall");
+    self.port.emit("uninstall");
   }
+
+  $scope.openModalPreview = function() {
+    let modal = $modal.open({
+      templateUrl: "dataPreview.html",
+      controller: ModalPreviewCtrl,
+      resolve: {
+        dispatchBatch: function() {
+          return $scope.dispatchBatch;
+        }
+      }
+    });
+  }
+
+  $scope.$on("dispatch_batch", function(evt, data) {
+    if(data) {
+      $scope.dispatchBatch = data;
+    }
+  });
 });
+
+let ModalPreviewCtrl = function($scope, $modalInstance, dispatchBatch) {
+  $scope.dispatchBatch = dispatchBatch;
+  $scope.prettified = false;
+
+  $scope.done = function() {
+    $modalInstance.close();
+  }
+
+  $scope.selectText = function() {
+    let elem = document.querySelector("#previewText");
+    if (elem) {
+      let range = document.createRange();
+      let sel = window.getSelection();
+      range.selectNodeContents(elem);
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
+  }
+}
 
 self.port.on("style", function(file) {
   let link = document.createElement("link");
