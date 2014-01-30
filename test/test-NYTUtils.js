f521863468f6df54deb1072199a4d82f88ec74e1
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-01-30 08:31:14 -0800

    Add NYTUtils object to encapsulate NYT userdata collection

---
+++ test/test-NYTUtils.js
@@ -0,0 +1,58 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const {Cc, Ci, Cu} = require("chrome");
+Cu.import("resource://gre/modules/Task.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://gre/modules/commonjs/sdk/core/promise.js");
+
+const {storage} = require("sdk/simple-storage");
+const {nsHttpServer, startServerAsync} = require("sdk/test/httpd");
+const test = require("sdk/test");
+const {NYTUtils} = require("NYTUtils");
+const {testUtils} = require("./helpers");
+
+exports["test fetchNYTUserData"] = function test_fetchNYTUserData(assert, done) {
+  Task.spawn(function() {
+    let server = new nsHttpServer();
+    server.start(-1);
+    let serverPort = server.identity.primaryPort
+    let serverUrl = "http://localhost:" + serverPort + "/post";
+    let responseJSON = {
+        "meta": {},
+        "data": {
+          "id": "0",
+          "name": "",
+          "subscription": {
+            "web": "0",
+            "mobile": "0",
+            "crosswords": "0",
+            "hd": "0"
+          }
+        }
+    };
+
+    NYTUtils._nytUserDataUrl = serverUrl;
+
+    // test registration
+    server.registerPathHandler("/post", (request, response) => {
+      assert.equal(request.method, "GET");
+      response.setHeader("Content-Type", "application/json; charset=utf8", false);
+      response.setStatusLine(request.httpVersion, 200, "OK");
+      response.write(JSON.stringify(responseJSON));
+    });
+
+    yield NYTUtils.fetchNYTUserData();
+    let userInfo = NYTUtils.getNYTUserData();
+    server.stop(function(){});
+    testUtils.isIdentical(assert, userInfo, responseJSON);
+  }).then(done);
+}
+
+test.run(exports);
