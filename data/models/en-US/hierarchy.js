094134f7c5a2a59436cd81e44c7d7e5c751f4a05
Marina Samuel <msamuel@mozilla.com>
2014-07-10 15:33:18 -0400

    Add file describing category hierarchy.

---
+++ data/models/en-US/hierarchy.js
@@ -0,0 +1,61 @@
+let hierarchy = {
+	"Arts": {
+		"Arts": {},
+		"Design": {},
+		"Ideas": {},
+		"Movies": {},
+		"Music": {},
+		"Photography": {},
+		"Television": {}
+	},
+	"Business": {
+		"Business": {},
+		"Entrepreneur": {}
+	},
+	"Computers": {
+		"Android": {},
+		"Apple": {},
+		"Programming": {},
+		"Technology": {}
+	},
+	"Games": {
+		"Video-Games": {}
+	},
+	"Health": {
+		"Health-Men": {},
+		"Health-Women": {}
+	},
+	"Home": {
+		"Cooking": {},
+		"Do-It-Yourself": {},
+		"Gardening": {},
+		"Home-Design": {}
+	},
+	"Recreation": {
+		"Autos": {},
+		"Humor": {},
+		"Travel": {}
+	},
+	"Science": {
+		"Science": {}
+	},
+	"Society": {
+		"Fashion-Women": {},
+		"Fashion-Men": {},
+		"Gossip": {},
+		"Politics": {},
+		"Weddings": {}
+	},
+	"Sports": {
+		"Baseball": {},
+		"Basketball": {},
+		"Boxing": {},
+		"Football": {},
+		"Golf": {},
+		"Hockey": {},
+		"Mixed-Martial-Arts": {},
+		"Soccer": {},
+		"Sports": {},
+		"Tennis": {}
+	}
+};
