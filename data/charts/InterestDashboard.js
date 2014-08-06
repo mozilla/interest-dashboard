184d73c9eddad4c26ee93655c8fdd3ac9a16aa9d
Marina Samuel <msamuel@mozilla.com>
2014-08-06 12:39:29 -0400

    Add data processing and graphing of area chart.

--- data/charts/InterestDashboard.js
+++ data/charts/InterestDashboard.js
@@ -1,23 +1,53 @@
 function InterestDashboard() {
-  this._chart = nv.models.pieChart()
+  /* Initing the pie chart */
+  this._pieChart = nv.models.pieChart()
       .showLegend(false)
       .x(function(d) { return d.label })
       .y(function(d) { return d.value })
       .showLabels(false);
 
   nv.addGraph(function() {
-    return this._chart;
+    return this._pieChart;
+  });
+
+  /* Initing the area graph */
+  this._areaGraph = nv.models.stackedAreaChart()
+                .margin({right: 100})
+                .x(function(d) { return d[0] })
+                .y(function(d) { return d[1] })
+                .useInteractiveGuideline(true)
+                .showLegend(false)
+                .showControls(false)
+                .transitionDuration(300);
+
+  this._areaGraph.xAxis
+    .tickFormat((d) => { return d3.time.format('%x')( new Date(d)); });
+
+  this._areaGraph.yAxis
+    .tickFormat((d) => { return d; });
+
+  nv.addGraph(() => {
+    return this._areaGraph;
   });
 }
 
 InterestDashboard.prototype = {
   graph: function(data) {
     d3.select('#interestPie').selectAll("*").remove();
+    d3.select('#areaGraph').selectAll("*").remove();
 
-    let div = d3.select("#interestPie")
+    d3.select("#interestPie")
       .attr("class", "margin-fix")
-      .datum(data)
+      .datum(data.pieData)
       .transition().duration(350)
-      .call(this._chart);
+      .call(this._pieChart);
+
+    d3.select("#areaGraph")
+      .datum(data.areaData)
+      .transition().duration(350)
+      .call(this._areaGraph);
+
+    nv.utils.windowResize(this._areaGraph.update);
+    nv.utils.windowResize(this._pieChart.update);
   }
 }
