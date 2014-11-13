6e130d59157568f35174f6af17beb38b18861d29
Matthew Ruttley <mruttley@mozilla.com>
2014-11-12 16:46:49 -0800

    Parse URI ready for testing

--- data/licaWorker.js
+++ data/licaWorker.js
@@ -1,45 +1,46 @@
-function processURL(url){
-  //Accepts: a url string
-  //Returns: a classification
-  
-  //extract different types of chunks
-  chunks = {
-    "protocol": "",
-    "subdomains": [],
-    "domain_name": "",
-    "path": [],
-    "variable_names": [],
-    "variable_values": [],
-  }
-  
-  //iterate through each letter. This avoids endless costly splits
-  buffer = ""
-  position = 0
-  while(true){
-	add_to_buffer = true
-	letter = url[position]
+
+
+function getDomain(url){
+	//accepts a url
+	//returns the domain name + TLD
+	return domain
+}
+
+function parseURI(url){
+	//Accepts: a url string
+	//Returns: different parts of the URI
 	
-	//check for http/s
-    if (letter == "/"){
-		if ((url[position-1] == "/") && (url[position[-2]])) {
-			chunks[protocol] = buffer
-			buffer = ""
-			add_to_buffer = false
+	//extract different types of chunks
+	chunks = {
+		"protocol": "",
+		"subdomains": [],
+		"domain_name": "",
+		"path": [],
+		"filename": "",
+		"variable_names": [],
+		"variable_values": [],
+	}
+
+	//this could be made more efficient with a letter-by-letter while loop
+	chunks['protocol'] = url.split("://")[0]
+	chunks['domain_name'] = get_domain(url)
+	chunks['subdomains'] = url.split(chunks['domain_name'])[0].split(chunks['protocol'])[1].split(".")
+	
+	path_and_rest = url.split(chunks['domain_name'])[1].split("?")
+	path_items = path_and_rest[0].split("/")
+	chunks['path'] = path_items.slice(0,-1)
+	chunks['filename'] = path_items.slice(-1)
+	
+	if (path_and_rest.length > 1) {
+		variables = path_and_rest[1].split("&")
+		for (let vq of variables) {
+			vq = vq.split("=")
+			variable_names.append(vq[0])
+			if (vq.length > 1) {
+				variable_values.append(vq[1])
+			}
 		}
 	}
 	
-	//chop at ?
-	if (test) {
-		//code
-	}
-	
-	if (add_to_buffer) {
-		buffer += letter
-	}
-}
-  
-  chunks['protocol'] = url.split("://")[0]
-  full_domain = url.split("://")[1].split("/")[0]
-  
-  
+	return chunks
 }
