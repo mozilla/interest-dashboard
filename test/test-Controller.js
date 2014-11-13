0a8408c3b2d891734ac0f2fb8db00de11b57d47d
Marina Samuel <msamuel@mozilla.com>
2014-11-13 11:42:37 -0800

    Fixing Controller tests and removing unused pieces of Controller.

--- test/test-Controller.js
+++ test/test-Controller.js
@@ -22,12 +22,10 @@ const {testUtils} = require("./helpers");
 const {promiseTimeout} = require("Utils");
 const {Stream} = require("streams/core");
 const {DayCountRankerBolt} = require("streams/dayCountRankerBolt");
-const {HostStripBolt} = require("streams/hostStripBolt");
-const {InterestStorageBolt} = require("streams/interestStorageBolt");
 
 let setupTestController = testUtils.setupTestController;
 
-exports["test controller"] = function test_Controller(assert, done) {
+exports["test controller history submission completes"] = function test_Controller(assert, done) {
   Task.spawn(function() {
     try {
       yield testUtils.promiseClearHistory();
@@ -43,70 +41,32 @@ exports["test controller"] = function test_Controller(assert, done) {
       let testController = setupTestController();
       testController.clear();
 
-      let processDeferred;;
-
+      let processDeferred;
       processDeferred = oldPromise.defer();
-      testController._streamObjects.rankerBolts[0].setEmitCallback(bolt => {
-        if (bolt.storage.ranking[bolt.storageKey].Autos == 4) {
-          processDeferred.resolve();
-        }
+      testController._streamObjects.chartDataProcessorBolt.setEmitCallback(bolt => {
+        assert.equal(Object.keys(bolt.storage.domains.all).length, 1, "Test we have the autoblog domain");
+        processDeferred.resolve();
       });
-      yield testController.submitHistory({flush: true});
-      yield processDeferred.promise;
-      testController._streamObjects.dailyInterestsSpout.setEmitCallback(undefined);
 
-      // wait till storage bolt has captured the interests
-      processDeferred = oldPromise.defer();
-      testController._streamObjects.interestStorageBolt.setEmitCallback(bolt => {
-        if (Object.keys(bolt.storage.interests).length == 4) {
-          processDeferred.resolve();
-        }
-      });
-      yield processDeferred.promise;
-      testController._streamObjects.interestStorageBolt.setEmitCallback(undefined);
-
-      let payload = testController.getNextDispatchBatch();
-      let days = Object.keys(payload.interests);
-      // make sure that the history data is keyed on 4,5, and 6 th day
-      assert.deepEqual(days ,  ["" + (today-4), "" + (today-3), "" + (today-2), "" + today], "4 days upto today");
-
-      // add one more visits for today and make sure we pick them up
-      yield testUtils.promiseAddVisits({uri: NetUtil.newURI("http://www.thehill.com/"), visitDate: microNow + 1});
-      yield testUtils.promiseAddVisits({uri: NetUtil.newURI("http://www.rivals.com/"), visitDate: microNow + 2});
-      yield testUtils.promiseAddVisits({uri: NetUtil.newURI("http://www.rivals.com/"), visitDate: microNow + MICROS_PER_DAY});
-
-
-      processDeferred = oldPromise.defer();
       let observer = {
         observe: function(aSubject, aTopic, aData) {
           try {
             if  (aTopic != "controller-history-submission-complete") {
               throw "UNEXPECTED_OBSERVER_TOPIC " + aTopic;
             }
-            processDeferred.promise.then(() => {
-              testController._streamObjects.dailyInterestsSpout.setEmitCallback(undefined);
-
-              // and we must see 4 day in the keys
-              payload = testController.getNextDispatchBatch();
-              days = Object.keys(payload.interests);
-              assert.deepEqual(days ,  ["" + (today-4), "" + (today-3), "" + (today-2), "" + today],"still 4 days");
-              Services.obs.removeObserver(observer, "controller-history-submission-complete");
-              done();
-            });
+            testController._streamObjects.dailyInterestsSpout.setEmitCallback(undefined);
+            Services.obs.removeObserver(observer, "controller-history-submission-complete");
+            assert.ok(true);
+            done();
           } catch (ex) {
             console.error(ex);
           }
         },
       };
 
-      testController._streamObjects.rankerBolts[0].setEmitCallback(bolt => {
-        if (Object.keys(bolt.storage.ranking[bolt.storageKey]).length == 3) {
-          processDeferred.resolve();
-        }
-      });
-
       Services.obs.addObserver(observer, "controller-history-submission-complete" , false);
-      Services.obs.notifyObservers(null, "idle-daily", null);
+
+      yield testController.submitHistory({flush: true});
     } catch(ex) {
       console.error(ex);
     }
@@ -126,25 +86,28 @@ exports["test stop and start"] = function test_StopAndStart(assert, done) {
       yield testUtils.addVisits(hostArray,59);
 
       let storageBackend = {};
-      let testController = setupTestController({storage: storageBackend});
+      let testController = setupTestController({
+        storage: storageBackend,
+        historyDays: 90,
+      });
       testController.clear();
 
       let processDeferred;
-
       processDeferred = oldPromise.defer();
-      testController._streamObjects.interestStorageBolt.setEmitCallback(bolt => {
-        if (Object.keys(bolt.storage.interests).length == 60) {
+      testController._streamObjects.chartDataProcessorBolt.setEmitCallback(bolt => {
+        let minDay = bolt.storage.chartData.genericChartData.lwca["58-cat"].minDay;
+        let maxDay = bolt.storage.chartData.genericChartData.lwca["58-cat"].maxDay;
+        let numDays = maxDay - minDay + 1;
+        if (numDays == 60) {
           processDeferred.resolve();
         }
       });
 
       yield testController.submitHistory({flush: true});
       yield processDeferred.promise;
-      testController._streamObjects.interestStorageBolt.setEmitCallback(undefined);
 
       let today = DateUtils.today();
       let theVeryLastTimeStamp = storageBackend.lastTimeStamp;
-
       // a toture test to make sure we can disable and re-enable
       // controller without loss of data
       testController.clear();
@@ -169,7 +132,6 @@ exports["test stop and start"] = function test_StopAndStart(assert, done) {
     }
   }).then(done);
 }
-
 
 exports["test clear storage"] = function test_ClearStorage(assert, done) {
   Task.spawn(function() {
@@ -198,66 +160,6 @@ exports["test clear storage"] = function test_ClearStorage(assert, done) {
       assert.equal(storageBackend.interests, undefined);
       assert.equal(storageBackend.ranking, undefined);
       assert.equal(storageBackend.hasOwnProperty("interests"), false);
-    } catch(ex) {
-      console.error(ex);
-    }
-  }).then(done);
-}
-
-exports["test get uuid"] = function test_GetUUID(assert, done) {
-  Task.spawn(function() {
-    try {
-      let testController = setupTestController();
-      assert.ok(testController.getUserID() != null);
-      assert.ok(testController.getUserID() != "");
-    } catch(ex) {
-      console.error(ex);
-    }
-  }).then(done);
-}
-
-exports["test mayPersonalize"] = function test_MayPersonalize(assert, done) {
-  Task.spawn(function() {
-    try {
-      // set uuid andt make a controller
-      simplePrefs.prefs.uuid = "1";
-      let storage = {};
-      let testController = setupTestController({storage: storage});
-      let pObject = testController._dispatcher._makePayloadObject();
-      assert.ok(testController.mayPersonalize() == true);
-      assert.ok(pObject.personalizeOn == true);
-
-      // remake controller for a different uuid
-      simplePrefs.prefs.uuid = "2";
-      testController = setupTestController({storage: storage});
-      assert.ok(testController.mayPersonalize() == false);
-      pObject = testController._dispatcher._makePayloadObject();
-      assert.ok(pObject.personalizeOn == false);
-    } catch(ex) {
-      console.error(ex);
-    }
-  }).then(done);
-}
-
-exports["test hostComputedInterests"] = function test_HostComputedInterests(assert, done) {
-  Task.spawn(function() {
-    try {
-      let hostArray = ["www.autoblog.com",
-                       "www.thehill.com",
-                       "www.mysql.com",
-                      ];
-      yield testUtils.promiseClearHistory();
-      yield testUtils.addVisits(hostArray,2);
-
-      let testController = setupTestController();
-      testController.clear();
-      yield testController.submitHistory({flush: true});
-      let results = testController.getHostComputedInterests();
-      assert.deepEqual(results, {
-        "1":{"interests":{"Autos":300},"frecency":300},
-        "2":{"interests":{"Autos":300,"Politics":300},"frecency":300},
-        "3":{"interests":{"Autos":300,"Politics":300,"Programming":300},"frecency":300}
-      });
     } catch(ex) {
       console.error(ex);
     }
