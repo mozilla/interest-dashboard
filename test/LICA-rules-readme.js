c3b62315e11bbd1018f8e64d2fca1e0fb6bf2fcb
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-11-14 15:54:21 -0800

    Added back LICA related worker and tests

---
+++ test/LICA-rules-readme.js
@@ -0,0 +1,26 @@
+
+//description method
+//URL for examples: https://www.www.e4hats.com/hats/ribbon-band-wool-hat-red.html?q=fedora
+
+//Mozilla URL description language
+
+// DEFINITIONS
+// //ftp (the protocol)
+// .www (the subdomain)
+// /hats (in the path)
+// -ribbon (in the filename)
+// ?q (a variable name)
+// =fedora (a variable value)
+// ?q=fedora (specifically q equal to fedora)
+// *wool (wool is anywhere in the URL)
+
+// NGRAMS
+// /real,estate (real followed by estate exists in the path)
+
+// MULTIPLES
+// .www{2} (www appears twice)
+// .www{3,} (www appears more than three times)
+
+// NEGATIVES
+// !.www (www is not in the subdomain)
+
