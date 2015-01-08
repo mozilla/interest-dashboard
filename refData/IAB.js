b2e9a530511c90ed9b4d54b96c04728d0bd4e577
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2015-01-07 16:10:25 -0800

    Remove , from category name

--- refData/IAB.js
+++ refData/IAB.js
@@ -45,7 +45,7 @@ var Taxonomy = {
 			"retirement planning": true,
 			"investing": true,
 			"banking": true,
-			"credit, debt & loans": true,
+			"credit & debt & loans": true,
 			"stocks": true,
 			"insurance": true,
 			"tax": true,
